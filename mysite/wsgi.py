655a49d81f850884892f98fc07f7f5ec8a4dc4d8
Faizdaud <ahmadfaizdaud007.com>
2021-05-18 14:46:51 +0800

    third commit

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = '/home/ahmadfaiz/my-first-django-blog' # use your own PythonAnywhere username here
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
